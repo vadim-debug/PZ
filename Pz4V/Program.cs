3298eace0d1cf752133c8f33e26ffaa991fbcb4a
Vadim Popov <v4dim-p0pov@yandex.ru>
2022-03-07 12:21:30 +0700

    Project

---
+++ Pz4V/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pz4V
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Session first = new Session();
+
+            first.SetExams(2, 6);
+            first.SetSemester("Первый");
+            first.PrintSession();
+
+            Session second = new Session();
+            second.SetSemester("второй");
+            second.GetSemester();
+            second.SetExams(7, 2);
+            second.CountExam();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Pz4V/Session.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pz4V
+{
+    public class Session
+    {
+        private string Semester;
+        public void GetSemester()
+        {
+            Console.WriteLine(Semester);
+        }
+
+        public void SetSemester(string S)
+        {
+            this.Semester = S;
+        }
+        private int Test;
+        private int Exam;
+        public void SetExams(int Test, int Exam)
+        {
+            this.Test = Test;
+            this.Exam = Exam;
+        }
+        public void CountExam()
+        {
+            int C = Exam + Test;
+            Console.WriteLine("Кол-во экзаменов и зачётов = " + C);
+        }
+        public void PrintSession()
+        {
+            Console.WriteLine("Семестр = " + Semester + " |" +
+                " Зачётов = " + Test + " |" +
+                " Екзаменов = " + Exam + " |");
+        }
+    }
+}
